058c079084d3fb7fa9b9758008fb2e52a15d7c27
Aleksey Obukhov <miptleha@gmail.com>
2024-04-23 16:58:46 +0300

    step 04

---
+++ steps/04/README.md
@@ -0,0 +1,20 @@
+## React application
+Let's take the project from the previous [step](../03) and create a simple react application.  
+Install the `React` libraries:
+```
+npm add react react-dom
+npm add -D @types/react @types/react-dom
+```
+
+Delete old files from the [src](src) folder and write a simple React application.  
+Replace the entry point in [webpack.config.ts](webpack.config.ts) with [src/index.tsx](src/index.tsx).  
+Set the `jsx` property to `react-jsx` value in the [tsconfig.json](tsconfig.json) file so that we do not need to import React in each tsx file.  
+Launch the already configured devServer:
+```
+npm start
+```
+The application is working.  
+![app](app.png)
+
+
+[Project source code](./)
---
+++ steps/04/webpack.config.ts
@@ -0,0 +1,47 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import type { Configuration } from "webpack";
+import type { } from "webpack-dev-server";
+
+type Mode = "production" | "development";
+interface EnvVariables {
+    mode: Mode
+}
+
+const configFunc = function (env: EnvVariables) {
+    const mode = env.mode ?? 'development';
+    const isDev = mode === 'development';
+
+    const config: Configuration = {
+        mode: mode,
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[contenthash].js',
+            clean: true
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        devtool: isDev && 'inline-source-map',
+        devServer: isDev ? {
+            port: 8080,
+            open: true
+        } : undefined,
+    }
+    return config;
+}
+
+export default configFunc;
