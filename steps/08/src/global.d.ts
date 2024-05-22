230bc66cd60f1e53779416d9901fd0402d8c8e4a
Aleksey Obukhov <miptleha@gmail.com>
2024-05-22 20:35:06 +0300

    svg declaration

--- steps/08/src/global.d.ts
+++ steps/08/src/global.d.ts
@@ -18,4 +18,9 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.gif';
 declare module '*.ico';
-declare module '*.svg';
+
+declare module "*.svg" {
+  import React from "react";
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
